224594d6da079585d16143e52326d16aa5709a83
Lanusse <thalita.lanusse@gmail.com>
2023-01-11 13:52:55 -0300

    BigBang

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nginx_hello_python',
+    version='0.1.0',
+    packages=find_packages(include=['app']),
+    test_suite = 'test',
+    install_requires=[
+        "flask >= 2.0.0"
+    ],
+    python_requires='>=3.6'
+)
